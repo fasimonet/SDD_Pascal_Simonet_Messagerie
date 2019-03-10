c27c230a509e62bb0773dcfa875e254dd0112bd4
Julien-Pascal <julien.p.fzs@gmail.com>
2019-03-10 20:57:46 +0100

    test fichier, fini

--- Code/manipulation_fichier.c
+++ Code/manipulation_fichier.c
@@ -3,7 +3,11 @@
 int main
 {
     // teste lecture de fichier avec élément de plus de 100 caractere
-    message_t     * t = lire_fichier(fichier_test_lecture)
-    afficher_LCH()
+    message_t     * t = lire_fichier(fichier_test_lecture);
+    afficher_LCH(*t);
+    // stockage dans un fichier
+    char        * nom;
+    fscanf(stdin,"%s", &nom);
+    sauv_fichier(nom,t);
     return 0;
 }
